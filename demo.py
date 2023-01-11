bc0df3fa30d34265bacdd2fa1ca0a01e0a34b858
Vishal Singh <55878408+vishalsingh17@users.noreply.github.com>
2023-01-11 21:14:13 +0530

    Updated demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 print("Line1")
-print("Line2")
+print("This is Line2")
 print("Line3")
 print("Line4")
 print("Line5")
 print("Line6")
+
+# Condition for merge conflict is that, the change should occurs in the same file and same line.
