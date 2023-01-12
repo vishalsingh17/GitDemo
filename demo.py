1c072f5a0b345588db14369fd87a7e18eaac2932
Vishal Singh <55878408+vishalsingh17@users.noreply.github.com>
2023-01-12 20:59:16 +0530

    Form

--- demo.py
+++
@@ -1,9 +0,0 @@
-print("Line1")
-print("This is Line2")
-print("This is changed by developer 2")
-print("Line3")
-print("Line4")
-print("Line5")
-print("Line6")
-
-# Condition for merge conflict is that, the change should occurs in the same file and same line.
--- dev.py
+++
@@ -1,4 +0,0 @@
-print("I'm currently on dev branch")
-print("Line 2")
-print("Line 3")
-print("Line 4")
